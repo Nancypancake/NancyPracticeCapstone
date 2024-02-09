b5963389020f2c77c267183281e0624bea473d79
loveskiddo <you@example.com>
2024-02-09 10:06:34 -0800

    #saving

--- app/utils/secrets copy.py
+++ app/utils/secrets copy.py
@@ -2,8 +2,8 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':"",
-        'MONGO_DB_NAME':"",
+        'MONGO_HOST':"mongodb+srv://jcoffee0604:coffee4life@cluster0.g3g4baj.mongodb.net/COFFEE?retryWrites=true&w=majority",
+        'MONGO_DB_NAME':"COFFEE",
         'GOOGLE_CLIENT_ID': "",
         'GOOGLE_CLIENT_SECRET':"",
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
